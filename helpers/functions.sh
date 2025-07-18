03352c9d0f481b8bcc4049bcc93040557d90ef63
Stefan Meyer <meyer.stefan1@gmx.net>
2025-07-18 10:36:42 +0200

    Update functions.sh

--- helpers/functions.sh
+++ helpers/functions.sh
@@ -1,1 +1,25 @@
+GITHUB="https://raw.githubusercontent.com/Q14siX/mailarchiva-installer-i18n/main/"
 
+get_system_language() {
+  local lang="${LC_ALL:-${LC_MESSAGES:-$LANG}}"
+  lang="${lang%%[_\.]*}"
+  [[ "$lang" == "de" ]] && LANGUAGE="de" || LANGUAGE="en"
+}
+
+add_trailing_slash() {
+  case "$1" in
+    */) echo "$1" ;;
+    *)  echo "$1/" ;;
+}
+
+source_remote() {
+  source <(wget -qO- "${GITHUB}$1")
+}
+
+source_local() {
+  source $1
+}
+
+source_remote "lang/${LANGUAGE}.lang"
+
+echo "Sprache $LANGUAGE"
