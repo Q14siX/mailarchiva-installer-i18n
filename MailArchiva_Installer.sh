e3276891defc7c6a9efaab4833f823d916b31d12
Stefan Meyer <meyer.stefan1@gmx.net>
2025-07-18 11:20:39 +0200

    Update MailArchiva_Installer.sh

--- MailArchiva_Installer.sh
+++ MailArchiva_Installer.sh
@@ -3,3 +3,10 @@
 GITHUB_RAW="https://raw.githubusercontent.com/Q14siX/mailarchiva-installer-i18n/main/"
 
 source <(wget -qO- "${GITHUB_RAW}helpers/functions.sh")
+
+get_system_language
+source_remote "lang/${LANGUAGE}.lang"
+
+echo "Sprache: $LANGUAGE"
+echo "Version: $(get_github_info tag_name)"
+echo "Name: $(get_github_info name)"
